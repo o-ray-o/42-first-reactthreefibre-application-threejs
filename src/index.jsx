38f72769cb9c19e0bc1adf79c9feb0a608fbfd7d
o-ray-o <rayfong_goh@mymail.sutd.edu.sg>
2023-12-27 15:41:19 +0800

    First commit

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+import * as THREE from "three";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(
+  <Canvas
+    dpr={[1, 2]}
+    gl={{
+      antialias: false,
+      toneMapping: THREE.ACESFilmicToneMapping,
+      outputEncoding: THREE.LinearEncoding,
+    }}
+    camera={{
+      fov: 45,
+
+      near: 0.1,
+      far: 200,
+      position: [3, 2, 6],
+    }}
+  >
+    <Experience />
+  </Canvas>
+);
